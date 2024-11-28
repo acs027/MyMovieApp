0924a9a00d4f5dca732b941b484ebac48b86901c
acs027 <alicihansarac@gmail.com>
2024-11-29 02:49:25 +0300

    Initial Commit

---
+++ MyMovieApp/MyMovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMovieAppApp.swift
+//  MyMovieApp
+//
+//  Created by ali cihan on 29.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct MyMovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
